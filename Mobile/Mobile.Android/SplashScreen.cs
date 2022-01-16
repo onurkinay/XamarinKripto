bddbfc927ed91d9722e2ab6ff033c630307ded72
Onur Kınay <ben@onurkinay.com.tr>
2022-01-16 13:43:04 +0300

    added icon and splash screen

---
+++ Mobile/Mobile.Android/SplashScreen.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Mobile.Droid
+{
+    [Activity(Label = "Kripto Portfolyo", Icon = "@drawable/icon", Theme = "@style/Theme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+
+    public class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+              
+            //Start Activity1 Activity  
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
